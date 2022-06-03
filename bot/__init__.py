bbe71bd09c7ab032b164381ecd491f093d6c57bb
istareatotherscode <91970414+istareatotherscode@users.noreply.github.com>
2022-06-03 15:33:24 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -53,7 +53,7 @@ except:
 
 PORT = environ.get('PORT', SERVER_PORT)
 Popen([f"gunicorn web.wserver:app --bind 0.0.0.0:{PORT}"], shell=True)
-srun(["qbittorrent-nox", "-d", "--profile=."])
+#srun(["qbittorrent-nox", "-d", "--profile=."])
 if not ospath.exists('.netrc'):
     srun(["touch", ".netrc"])
 srun(["cp", ".netrc", "/root/.netrc"])
